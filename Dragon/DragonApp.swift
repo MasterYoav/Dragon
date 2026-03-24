ccf0d87256e1a8f88974ec4353913411ccdcdecc
MasterYoav <realyoavperetz@gmail.com>
2026-03-24 06:58:03 +0200

    Initial Commit

---
+++ Dragon/DragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonApp.swift
+//  Dragon
+//
+//  Created by Yoav Peretz on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DragonTests/DragonTests.swift
@@ -0,0 +1,17 @@
+//
+//  DragonTests.swift
+//  DragonTests
+//
+//  Created by Yoav Peretz on 24/03/2026.
+//
+
+import Testing
+@testable import Dragon
+
+struct DragonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
